fcd7e62845ee40def444fd139161cc5571993e52
ManuelHeraz <hernandezdiazjm00@gmail.com>
2024-03-30 15:13:47 -0600

    v24.03.30.0313

---
+++ assets/js/music-list.js
@@ -0,0 +1,175 @@
+let allMusic = [
+    
+    {
+        name: "Flash FM Radio Station",//music name
+        artist: "Flash FM", // artist name
+        img:"FlashFMCover", // image name
+        src: "FlashFM1" //name here
+    },
+
+    {
+        name: "Out Of Touch",//music name
+        artist: "Flash FM", // artist name
+        img:"FlashFMCover", // image name
+        src: "FlashFM2-OutOfTouch" //name here
+    },
+
+    {
+        name: "Flash FM Radio Station",//music name
+        artist: "Flash FM", // artist name
+        img:"FlashFMCover", // image name
+        src: "FlashFM3" //name here
+    },
+
+    {
+        name: "Flash FM Radio Station",//music name
+        artist: "Flash FM", // artist name
+        img:"FlashFMCover", // image name
+        src: "FlashFM4-DanceHallDays" //name here
+    },
+
+
+    //FaltanImagenes
+
+    {
+        name: "Flash FM Radio Station",//music name
+        artist: "Flash FM", // artist name
+        img:"FlashFMCover", // image name
+        src: "FlashFM5" //name here
+    },
+
+    {
+        name: "Flash FM Radio Station",//music name
+        artist: "Flash FM", // artist name
+        img:"FlashFMCover", // image name
+        src: "FlashFM6-BillieJean" //name here
+    },
+
+    {
+        name: "Flash FM Radio Station",//music name
+        artist: "Flash FM", // artist name
+        img:"FlashFMCover", // image name
+        src: "FlashFM7-SelfControl" //name here
+    },
+
+    {
+        name: "Flash FM Radio Station",//music name
+        artist: "Flash FM", // artist name
+        img:"FlashFMCover", // image name
+        src: "FlashFM8" //name here
+    },
+
+    {
+        name: "Flash FM Radio Station",//music name
+        artist: "Flash FM", // artist name
+        img:"FlashFMCover", // image name
+        src: "FlashFM9-CallMe" //name here
+    },
+
+    {
+        name: "Flash FM Radio Station",//music name
+        artist: "Flash FM", // artist name
+        img:"FlashFMCover", // image name
+        src: "FlashFM10-KissTheDirt" //name here
+    },
+
+    {
+        name: "Flash FM Radio Station",//music name
+        artist: "Flash FM", // artist name
+        img:"FlashFMCover", // image name
+        src: "FlashFM11" //name here
+    },
+
+    {
+        name: "Flash FM Radio Station",//music name
+        artist: "Flash FM", // artist name
+        img:"FlashFMCover", // image name
+        src: "FlashFM12-RunToYou" //name here
+    },
+
+    {
+        name: "Flash FM Radio Station",//music name
+        artist: "Flash FM", // artist name
+        img:"FlashFMCover", // image name
+        src: "FlashFM13-OwnerOfALonelyHeart" //name here
+    },
+
+    {
+        name: "Flash FM Radio Station",//music name
+        artist: "Flash FM", // artist name
+        img:"FlashFMCover", // image name
+        src: "FlashFM14-VideoKilledTheRadioStar" //name here
+    },
+
+    {
+        name: "Flash FM Radio Station",//music name
+        artist: "Flash FM", // artist name
+        img:"FlashFMCover", // image name
+        src: "FlashFM15-JapaneseBoy" //name here
+    },
+
+    {
+        name: "Flash FM Radio Station",//music name
+        artist: "Flash FM", // artist name
+        img:"FlashFMCover", // image name
+        src: "FlashFM16" //name here
+    },
+
+    {
+        name: "Flash FM Radio Station",//music name
+        artist: "Flash FM", // artist name
+        img:"FlashFMCover", // image name
+        src: "FlashFM17-LifesWhatYouMakeIt" //name here
+    },
+
+    {
+        name: "Flash FM Radio Station",//music name
+        artist: "Flash FM", // artist name
+        img:"FlashFMCover", // image name
+        src: "FlashFM18-YourLove" //name here
+    },
+
+    {
+        name: "Flash FM Radio Station",//music name
+        artist: "Flash FM", // artist name
+        img:"FlashFMCover", // image name
+        src: "FlashFM19" //name here
+    },
+
+    {
+        name: "Flash FM Radio Station",//music name
+        artist: "Flash FM", // artist name
+        img:"FlashFMCover", // image name
+        src: "FlashFM20-SteppinOute" //name here
+    },
+
+    {
+        name: "Flash FM Radio Station",//music name
+        artist: "Flash FM", // artist name
+        img:"FlashFMCover", // image name
+        src: "FlashFM21" //name here
+    },
+
+    {
+        name: "Flash FM Radio Station",//music name
+        artist: "Flash FM", // artist name
+        img:"FlashFMCover", // image name
+        src: "FlashFM22-OneThingsLeadsToAnother" //name here
+    },
+
+    {
+        name: "Flash FM Radio Station",//music name
+        artist: "Flash FM", // artist name
+        img:"FlashFMCover", // image name
+        src: "FlashFM23" //name here
+    },
+
+    {
+        name: "Flash FM Radio Station",//music name
+        artist: "Flash FM", // artist name
+        img:"FlashFMCover", // image name
+        src: "FlashFM24-RunningWithTheNight" //name here
+    },
+        
+
+];
